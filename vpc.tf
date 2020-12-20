7d7ffbb275af01fe7b1ba6ecd3e2ab2a40f9c8bf
Cloudiar <cloudiartest@gmail.com>
2020-12-20 13:10:03 -0500

    Checking web hook

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "cloudiar_vpc" {
         Name = "Cloudiar-vpc-${terraform.workspace}"
     }
 }
+
+resource "aws_subnet" "cloudiar_subnet" {
+    cidr_block = "172.17.1.0/24"
+    vpc_id = aws_vpc.cloudiar_vpc.id
+
+    tags = {
+        Name = "Cloudiar-subnet-${terraform.workspace}"
+    }
+}
