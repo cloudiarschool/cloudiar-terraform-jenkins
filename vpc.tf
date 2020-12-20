9cb8c325c22e0a5f60843b94dc80ae090c38bb5f
Cloudiar <cloudiartest@gmail.com>
2020-12-20 11:44:33 -0500

    Fixed terraform vpc file

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "cloudiar_vpc" {
-    cidr_block = 
-    instance_tenancy = 
+    cidr_block = var.vpc_cidr
+    instance_tenancy = var.tenancy
 
     tags = {
         Name = "Cloudiar-vpc-${terraform.workspace}"
